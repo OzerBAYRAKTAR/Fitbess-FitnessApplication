c7776d83e3f56f5b11f007542e6b018fe2705dbe
Özer BAYRAKTAR <ozer.bayraktar6117@gmail.com>
2023-08-24 23:02:33 +0300

    bottom navigation design completed

--- app/src/main/java/com/bayraktar/healthybackandneck/ui/HomeActivity.kt
+++ app/src/main/java/com/bayraktar/healthybackandneck/ui/HomeActivity.kt
@@ -5,9 +5,18 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.bayraktar.healthybackandneck.R
 import com.bayraktar.healthybackandneck.databinding.ActivityHomeBinding
+import com.bayraktar.healthybackandneck.ui.Exercise.ExerciseFragment
+import com.bayraktar.healthybackandneck.ui.HomePage.HomePageFragment
+import com.bayraktar.healthybackandneck.ui.Profile.ProfileFragment
+import com.bayraktar.healthybackandneck.ui.Settings.SettingsFragment
+import com.bayraktar.healthybackandneck.ui.Statistics.StatisticsFragment
 import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
@@ -32,23 +41,36 @@ class HomeActivity : AppCompatActivity() {
         toogle.syncState()
 
         navigationView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener {item: MenuItem ->
+            var fragment: Fragment? = null
             when (item.itemId) {
                 R.id.mainSC ->{
+                    fragment = HomePageFragment()
+                    loadFragment(fragment)
                     true
                 }
                 R.id.exercisescreen ->{
+                    fragment = ExerciseFragment()
+                    loadFragment(fragment)
                     true
                 }
                 R.id.statisticscreen ->{
+                    fragment = StatisticsFragment()
+                    loadFragment(fragment)
                     true
                 }
                 R.id.profilescreen ->{
+                    fragment = ProfileFragment()
+                    loadFragment(fragment)
                     true
                 }
                 R.id.settings ->{
+                    fragment = SettingsFragment()
+                    loadFragment(fragment)
                     true
                 }
                 R.id.connect ->{
+                    fragment = HomePageFragment()
+                    loadFragment(fragment)
                     true
                 }
                 else -> {
@@ -58,5 +80,12 @@ class HomeActivity : AppCompatActivity() {
 
         })
 
+    }
+    private fun loadFragment(fragment: Fragment) {
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame,fragment).commit()
+        drawerLayout.closeDrawer(GravityCompat.START)
+        fragmentTransaction.addToBackStack(null)
     }
 }
